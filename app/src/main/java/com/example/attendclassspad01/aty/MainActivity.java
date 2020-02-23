36dbe7e4384213e6b2504c2b0935c71e7b9df388
chenhuigithub <1518571128@qq.com>
2020-02-23 22:51:38 +0800

    我的课堂（授课系列学生端）

---
+++ app/src/main/java/com/example/attendclassspad01/aty/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.attendclassspad01.aty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.attendclassspad01.R;
+
+/**
+ * 主界面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
